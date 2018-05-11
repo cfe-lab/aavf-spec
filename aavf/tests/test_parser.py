17c9b1c51011131d7596bd3586898a7fab599096
jeff-k <jeff@x-0.ca>
2018-05-11 14:31:10 -0700

    basic API sanity check

--- aavf/tests/test_parser.py
+++ aavf/tests/test_parser.py
@@ -1,9 +1,18 @@
-from aavf import Reader
+from aavf import Reader, Writer
 
 import unittest
 
-a = Reader("example.aavf")
-print(a.header)
-for r in a:
-    print(r)
+reader = Reader("example.aavf")
+
+old_records = []
+with open("/tmp/test.aavf", 'wb') as fd:
+    w = Writer(fd, header=reader.header)
+    for r in reader:
+        old_records.append(r)
+        w.writerecord(r)
+
+for r1, r2 in zip(Reader("/tmp/test.aavf"), old_records):
+    print(r1,r2)
+
+
 
